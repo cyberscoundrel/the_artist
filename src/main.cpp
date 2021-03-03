d56bb05c61840706b086a98846623a51f0f796bb
cyberscoundrel <cyberscoundrel@gmail.com>
2021-03-03 02:07:37 -0800

    project mostly setup

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+
+
+#include "glm/fwd.hpp"
+#include <iostream>
+
+#include <SDL.h>
+
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+
+
+int main(int argc, char * argv[])
+{
+	glewInit();
+	glm::vec3 v;
+	std::cout << "hi" << std::endl;
+	return 0;
+}
+
